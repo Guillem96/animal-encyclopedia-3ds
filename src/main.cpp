b8fb13de2f9f98691aa673731e734b1943f60a1c
Guillem96 <guillem.orellana@gmail.com>
2018-08-20 16:54:22 +0200

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <3ds.h>
-
-#include "models/animal.h"
-#include "csv-utils/CsvIterator.h"
-
-std::vector<Animal> readAnimals(void);
+#include "App.h"
 
 int main(int argc, char *argv[])
 {
-    gfxInitDefault();
-    consoleInit(GFX_TOP, NULL);
-
-    std::vector<Animal> animals = readAnimals();
-    printf("%d\n", (int)animals.size());
-    
-    
-    // Main loop
-    while (aptMainLoop())
-    {
-        gspWaitForVBlank();
-        gfxSwapBuffers();
-        hidScanInput();
-
-        // Your code goes here
-        u32 kDown = hidKeysDown();
-        if (kDown & KEY_START)
-            break; // break in order to return to hbmenu
-    }
-
-    gfxExit();
+    MainScreen app = App();
+    app.run();
     return 0;
-}
-
-std::vector<Animal> readAnimals()
-{   
-    std::vector<Animal> animals;
-
-    Result rc = romfsInit();
-    if (rc)
-    {
-        printf("romfsInit: %08lX\n", rc);
-    }
-    else
-    {
-        CsvIterator it("romfs:/animals", ",", true);
-                
-        while (it.hasNext())
-        {
-            std::vector<std::string> next = it.next();
-            Animal a = Animal();
-            a.fromCsv(next);
-            animals.push_back(a);
-        }
-    }
-    return animals;
 }
