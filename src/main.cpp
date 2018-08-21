77c30fac36707516d5f752fcc04760cc25afc756
Guillem96 <guillem.orellana@gmail.com>
2018-08-21 16:21:34 +0200

    Main ready for citro2d

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <3ds.h>
+#include <citro2d.h>
 
-int main(int argc, char* argv[]) {
-	gfxInitDefault();
-	consoleInit(GFX_TOP, NULL);
+int main(int argc, char *argv[])
+{
+    gfxInitDefault();
+    C3D_Init(C3D_DEFAULT_CMDBUF_SIZE);
+    C2D_Init(C2D_DEFAULT_MAX_OBJECTS);
+    C2D_Prepare();
 
-	printf("Hello, world!\n");
+    C3D_RenderTarget *top = C2D_CreateScreenTarget(GFX_TOP, GFX_LEFT);
 
-	// Main loop
-	while (aptMainLoop())
-	{
-		gspWaitForVBlank();
-		gfxSwapBuffers();
-		hidScanInput();
+    // Main loop
+    while (aptMainLoop())
+    {
+        hidScanInput();
 
-		// Your code goes here
-		u32 kDown = hidKeysDown();
-		if (kDown & KEY_START)
-			break; // break in order to return to hbmenu
-	}
+        u32 kDown = hidKeysDown();
+        if (kDown & KEY_START)
+            break; // break in order to return to hbmenu
 
-	gfxExit();
-	return 0;
+        C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+        C2D_TargetClear(top, C2D_Color32(0x68, 0xB0, 0xD8, 0xFF));
+        C2D_SceneBegin(top);
+
+        // TODO: Font Renders
+
+        C3D_FrameEnd(0);
+    }
+
+    C2D_Fini();
+    C3D_Fini();
+    gfxExit();
+    return 0;
 }
