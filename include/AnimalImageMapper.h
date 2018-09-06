aad11b2a29e64ca0407d0926ad796ae157c0e487
Guillem96 <guillem.orellana@gmail.com>
2018-09-06 14:22:02 +0200

    Image mapper

---
+++ include/AnimalImageMapper.h
@@ -0,0 +1,15 @@
+#include <map> 
+#include <string>
+
+class AnimalImageMapper
+{
+    private:
+        std::map<std::string, int> m_imgIds;
+
+        void fillMap(const std::string& path);
+    public:
+        AnimalImageMapper(const std::string& path);
+        ~AnimalImageMapper();
+
+        int getImageId(const std::string imageName);
+};
---
+++ src/animal-image-mapper/AnimalImageMapper.cpp
@@ -0,0 +1,43 @@
+#include "AnimalImageMapper.h"
+
+AnimalImageMapper::AnimalImageMapper(const std::string &path)
+{
+    fillMap(path);
+}
+
+AnimalImageMapper::~AnimalImageMapper()
+{
+}
+
+int AnimalImageMapper::getImageId(const std::string imageName)
+{
+    return m_imgIds[imageName];
+}
+
+void AnimalImageMapper::fillMap(const std::string& path)
+{
+    FILE *f;
+    f = fopen(path.c_str(), "r");
+
+    if (f != NULL)
+    {
+        printf("Reading: %s\n", path.c_str());
+        
+        char line[512];
+        int id = 0;
+        fgets(line, 512, f); // Skip first line
+
+        while (fgets(line, 512, f) != NULL)
+        {
+            std::string name(line);
+            name = name.erase(name.find_last_not_of("\t\n\v\f\r ") + 1);
+            m_imgIds[name] = id;
+            id++;
+        }
+        fclose(f);
+    }
+    else
+    {
+        printf("ERROR: Reading file %s\n", path.c_str());
+    }
+}
