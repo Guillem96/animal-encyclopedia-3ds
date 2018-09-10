dfb305615a24c506eb3fec707152bf19622d2a14
Guillem Orellana <guillem.orellana@gmail.com>
2018-09-10 16:39:13 +0200

    Thread for each screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,38 @@
-#include "screen/App.h"
+#include "App.h"
+#include "AppBottom.h"
+
+#define STACKSIZE (4 * 1024)
+
+void top(void *arg);
+void bottom(void *arg);
 
 int main(int argc, char *argv[])
 {
+    Thread bottomThread, topThread;
+    
+    s32 mainPriority = 0;
+    svcGetThreadPriority(&mainPriority, CUR_THREAD_HANDLE);
+
+    topThread = threadCreate(top, NULL, STACKSIZE, mainPriority + 1, -2, false);
+    bottomThread = threadCreate(bottom, NULL, STACKSIZE * 10, mainPriority + 1, -2, false);
+
+    threadJoin(topThread, U64_MAX);
+    threadFree(topThread);
+
+    threadJoin(bottomThread, U64_MAX);
+    threadFree(bottomThread);
+
+    return 0;
+}
+
+void top(void *arg)
+{
     App app(Color(245.0f/255.0f, 245.0f/255.0f, 245.0f/255.0f, 1.0f));
     app.run();
-    return 0;
+}
+
+void bottom(void *arg)
+{
+    AppBottom app(Color(1.0f, 1.0f, 1.0f, 1.0f));
+    app.run();
 }
